bfddfcbbf1112694fe572682f3946b06027ecfa2
hari <harikrish123abc@gmail.com>
2024-09-18 12:22:36 +0530

    created a grade

---
+++ grade.js
@@ -0,0 +1,34 @@
+const grade=103
+if(grade>=0 && grade<=100) {
+
+if (grade>=90 && grade<=100 ){
+console.log("A+");
+
+}
+else if (grade>=80 && grade<90) {
+    console.log( "A");
+
+}
+else if (grade>=70 && grade<80 ){
+    console.log("B+");
+    
+}
+else if (grade>=60 && grade<70) {
+    console.log( "B");
+
+}
+else if (grade>=50 && grade<60){
+    console.log( "C");
+
+}
+
+else if (grade>=0 && grade<50) {
+    console.log( "F");
+
+}
+
+}
+else{
+    console.log("*****error messege*****")
+}
+ 
